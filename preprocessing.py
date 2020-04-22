da65e163c975f532b65e1110bef674b89aff3349
Shivayogi Biradar <shivayogibiradar@Shivayogis-MacBook-Pro.local>
2020-04-22 01:32:43 -0400

    preprocess

--- preprocessing.py
+++ preprocessing.py
@@ -43,7 +43,7 @@ def data_clean(path):
     for x in catagory_features:
         data[x] = data[x].fillna('None').astype('category')
     for y in number_features:
-        data[y] = data[y].fillna(0).astype(np.float)
+        data[y] = data[y].fillna(data[y].median()).astype(np.float)
     return data
 
 
@@ -63,48 +63,3 @@ def data_fillna(data):
         data[y] = data[y].fillna(0.0).astype(np.float)
     return data
 
-def append_data(data1, data2):
-    result_data = np.append(data1, data2, 1)
-    return result_data
-    
-def preprocessing_numerical_minmax(data):
-   
-    scaled_data = min_max_scaler.fit_transform(data)
-    return scaled_data
-    
-def preprocessing_categorical(data):
-    label_encoder = LabelEncoder()
-    label_encoded_data = label_encoder.fit_transform(data) 
-    label_binarizer = preprocessing.LabelBinarizer()
-    label_binarized_data = label_binarizer.fit_transform(label_encoded_data) 
-    return label_binarized_data
-
-def preprocessing_text(data):  
-    tfidf_vectorizer = TfidfVectorizer()
-    tfidf_vectorized_text = tfidf_vectorizer.fit_transform(data)  
-    return tfidf_vectorized_text
-def preprocessing_catagory(data):
-    data_c=0
-    for i in range(len(catagory_features)):
-        new_data = data[catagory_features[i]]
-        new_data_c = preprocessing_categorical(new_data)
-        if i == 0:
-            data_c=new_data_c
-        else:
-            data_c = np.append(data_c, new_data_c, 1)
-    return data_c
-
-def preprocessing_numerical(data):
-    data_list_numerical = list(zip(data['director_facebook_likes'], data['actor_1_facebook_likes'], 
-                                   data['actor_2_facebook_likes'], data['actor_3_facebook_likes'], 
-                                   data['cast_total_facebook_likes'], data['budget']))
-
-    data_numerical = np.array(data_list_numerical)
-    data_numerical = preprocessing_numerical_minmax(data_numerical)
-    return data_numerical
-
-def preprocessed_agregated_data(database): 
-    numerical_data = preprocessing_numerical(database)
-    categorical_data = preprocessing_catagory(database)
-    all_data = np.append(numerical_data, categorical_data, 1)
-    return all_data
